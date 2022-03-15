f86fa07a5d78eaec57555425ec16b34c458cd517
SomaKamikubo <e195708@ie.u-ryukyu.ac.jp>
2022-03-16 00:53:17 +0900

    クラス図再設計

---
+++ Assets/Script/MVP/UnityEvents.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//public class UnityEvents : MonoBehaviour :UnityEvent<PlayerState>
+//{
+//}
+public sealed class BoolEvent : UnityEvent<bool>
+{
+}
+
+public sealed class FloatEvent : UnityEvent<float>
+{
+}
+
