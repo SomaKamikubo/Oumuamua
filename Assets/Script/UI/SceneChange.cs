839da93689b364c44ebc568d5029249153e92688
SomaKamikubo <e195708@ie.u-ryukyu.ac.jp>
2022-03-21 10:24:55 +0900

    タイトル画面の作成

--- Assets/Script/UI/SceneChange.cs
+++ Assets/Script/UI/SceneChange.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class go_title : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
-
-    public void OnButtonPressed()
+    public void ChangeSceneTitle()
     {
         SceneManager.LoadScene("Title");
+    }
+
+    public void ChangeSceneMain()
+    {
+        SceneManager.LoadScene("main");
     }
 }
