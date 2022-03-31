1653b7bf5df836c878eea5d0868ffa211867bb42
SomaKamikubo <e195708@ie.u-ryukyu.ac.jp>
2022-03-31 23:44:49 +0900

    game_over view

--- Assets/Script/View/GameOver.cs
+++ Assets/Script/View/GameOver.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void GameOverView()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    
 }
