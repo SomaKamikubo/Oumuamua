57256b0ff2c66a96345bffe6a6f19170b873b950
shimayuki01 <66156828+shimayuki01@users.noreply.github.com>
2022-05-12 16:38:35 +0900

    ハートを自動生成できるようにした

--- Assets/Script/Chractor/Player/PlayerHurt.cs
+++ Assets/Script/Chractor/Player/PlayerHurt.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class PlayerHurt : MonoBehaviour
 {
-    [SerializeField] GameObject HP1;
-    [SerializeField] GameObject HP2;
-    [SerializeField] GameObject HP3;
+    [SerializeField] GameObject HP;
+
+    [SerializeField] int _max_hp;
+    [SerializeField] int _now_hp;
+    Vector3 _pos;
+    [SerializeField] float _add_pos;
+
+    [SerializeField] GameObject[] heart;
+    [SerializeField] GameObject hart;
+
+    private void Awake()
+    {
+        heart = new GameObject[_max_hp];
+        _pos = HP.transform.position;
+
+        heart[0] = HP;
+        for (int i=1; i < _max_hp; i++)
+        {
+            _pos.x += _add_pos;
+            heart[i] = Instantiate(HP, _pos, Quaternion.identity, hart.transform);
+
+        }
+    }
+
 
     //‚ ‚ÙƒR[ƒh
     public void ViewHurt(int hp)
     {
-        if (hp == 2)
-        {
-            HP3.SetActive(false);
-        }
-        if (hp == 1)
-        {
-            HP2.SetActive(false);
-        }
-        if (hp == 0)
-        {
-            HP1.SetActive(false);
-        }
+        
     }
 }
