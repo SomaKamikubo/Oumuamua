e522dc32afe4165122d9412e686d0831571ae157
shimayuki01 <66156828+shimayuki01@users.noreply.github.com>
2022-05-29 15:22:01 +0900

    ゲームオーバーイベント発行をゲームマネージャーに

--- Assets/Script/GameManage/GameManager.cs
+++ Assets/Script/GameManage/GameManager.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] PlayerWindow _playerWindow;
+
+    Subject<Unit> _gameOver = new Subject<Unit>();
+    public IObservable<Unit> OnGameOver { get { return _gameOver; } }
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //ハート生成
         _playerWindow.GenerateHeart();
+
+        //ゲームオーバーの発行
+        _playerWindow.OnDeath.Subscribe(_ => _gameOver.OnNext(default));
     }
 
     // Update is called once per frame
